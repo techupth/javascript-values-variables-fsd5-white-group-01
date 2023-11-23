ff80a10b4fbafbd00727a804c17ef25bf6666c11
NapatKan <147288328+NapatKan@users.noreply.github.com>
2023-11-23 13:04:29 +0700

    submit the exercise

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,17 @@
 //Exercise #1: Variable Creation
 //Start coding here
 
+let name = "Karter";
+let surname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const storeName = "Chef's Table by Chef James";
+const totalBill = 15000;
+const totalStore = 1500;
+
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalBill = 5000;
+totalStore = 2500;
